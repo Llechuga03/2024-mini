5972fbf6278eb66c3b914bb2aca5dd69fd763e99
Llechuga03 <140262648+Llechuga03@users.noreply.github.com>
2024-09-17 19:09:54 -0500

    Update exercise_light.py
    This work was all done by Rena, she was just having issues with pushing updates to the repository, so I'm doing it for her.

--- assignment/exercise_light.py
+++ assignment/exercise_light.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """
-Use analog input with photocell
+Use analog input with photoresistor to control the apparent brightness of the LED.
 """
 
 import time
 import machine
 
-# GP28 is ADC2
 ADC2 = 28
 
 led = machine.Pin("LED", machine.Pin.OUT)
@@ -14,32 +13,30 @@ adc = machine.ADC(ADC2)
 
 blink_period = 0.1
 
-max_bright = 20000
+max_bright = 17000
 min_bright = 10000
 
 
 def clip(value: float) -> float:
-    """clip number to range [0, 1]"""
+    """Clip number to range [0, 1]"""
     if value < 0:
         return 0
     if value > 1:
         return 1
     return value
 
-
 while True:
     value = adc.read_u16()
     print(value)
-    """
-    need to clip duty cycle to range [0, 1]
-    this equation will give values outside the range [0, 1]
-    So we use function clip()
-    """
 
     duty_cycle = clip((value - min_bright) / (max_bright - min_bright))
 
-    led.high()
-    time.sleep(blink_period * duty_cycle)
+    if duty_cycle == 0:
+       
+        led.low()
+    else:
+        led.high()
+        time.sleep(blink_period * duty_cycle)
 
-    led.low()
-    time.sleep(blink_period * (1 - duty_cycle))
+        led.low()
+        time.sleep(blink_period * (1 - duty_cycle))
