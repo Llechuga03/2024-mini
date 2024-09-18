0f65876c8de0ae1d4cfb93eefb05db0809d363c1
Renad-Alanazi <115852421+Renad-Alanazi@users.noreply.github.com>
2024-09-17 20:28:30 -0400

    Update exercise_game.py

--- assignment/exercise_game.py
+++ assignment/exercise_game.py
@@ -1,102 +1,119 @@
-"""
-Response time - single-threaded
-"""
-
+import network
 from machine import Pin
 import time
 import random
 import json
+import urequests  
+
+N = 10  
+on_ms = 500  
+
+wific = 'Apt214'
+wifi_pass = 'Apt214123'
 
 
-N: int = 3
-sample_ms = 10.0
-on_ms = 500
+conc = network.WLAN(network.STA_IF)
+conc.active(True)
+conc.connect(wific, wifi_pass)
 
+timeout = 10  
+start_time = time.time()
+while not conc.isconnected():
+    if time.time() - start_time > timeout:
+        print("Could not connect to Wi-Fi")
+        break
+    time.sleep(1)
+
+url_link= "https://ec463-74dcf-default-rtdb.firebaseio.com/response_times.json"
 
 def random_time_interval(tmin: float, tmax: float) -> float:
-    """return a random time interval between max and min"""
     return random.uniform(tmin, tmax)
 
-
-def blinker(N: int, led: Pin) -> None:
-    # %% let user know game started / is over
-
-    for _ in range(N):
+def blinker(led: Pin, times: int):
+    for _ in range(times):
         led.high()
         time.sleep(0.1)
         led.low()
         time.sleep(0.1)
 
-
-def write_json(json_filename: str, data: dict) -> None:
-    """Writes data to a JSON file.
-
-    Parameters
-    ----------
-
-    json_filename: str
-        The name of the file to write to. This will overwrite any existing file.
-
-    data: dict
-        Dictionary data to write to the file.
-    """
-
+def write_json(json_filename: str, data: dict):
     with open(json_filename, "w") as f:
         json.dump(data, f)
 
 
-def scorer(t: list[int | None]) -> None:
-    # %% collate results
-    misses = t.count(None)
-    print(f"You missed the light {misses} / {len(t)} times")
+def scorer(times: list):
+    t_good= [x for x in times if x is not None]  
+   
+    if t_good:
+        mini= min(t_good)
+        maxi= max(t_good)
+        avg= sum(t_good) / len(t_good)
+    else:
+        mini= maxi= avg= None
 
-    t_good = [x for x in t if x is not None]
+   
+    data = {
+        "minimum time:": mini,
+        "maximum time:": maxi,
+        "average time:": avg,
+        "misses:": times.count(None),
+        "num of flashes:": len(times),
+        "successful presses:": len(t_good),
+        "score:": len(t_good) / len(times) if times else 0
+    }
 
-    print(t_good)
+   
+    cur= time.localtime()
+    curt= "-".join(map(str,cur[:3])) +"T"+ "_".join(map(str, cur[3:6]))
+    fil= f"response_times_{curt}.json"
+    write_json(fil,data)
+   
+    return data  
 
-    # add key, value to this dict to store the minimum, maximum, average response time
-    # and score (non-misses / total flashes) i.e. the score a floating point number
-    # is in range [0..1]
-    data = {}
 
-    # %% make dynamic filename and write JSON
-
-    now: tuple[int] = time.localtime()
-
-    now_str = "-".join(map(str, now[:3])) + "T" + "_".join(map(str, now[3:6]))
-    filename = f"score-{now_str}.json"
-
-    print("write", filename)
-
-    write_json(filename, data)
-
+def upload_to_cloud(data: dict):
+    try:
+        r1 = urequests.post(url_link, json=data)
+        print("status code:", r1.status_code)
+        print("text:", r1.text)
+        r1.close()  
+    except OSError as e:
+        print("wifi error:", str(e))
+    except ValueError as e:
+        print("Data error:", str(e))
+    except Exception as e:
+        print("didnt upload:", str(e))
 
 if __name__ == "__main__":
-    # using "if __name__" allows us to reuse functions in other script files
-
     led = Pin("LED", Pin.OUT)
     button = Pin(16, Pin.IN, Pin.PULL_UP)
+   
+    t2 = []
 
-    t: list[int | None] = []
+    blinker(led, 3)  
 
-    blinker(3, led)
-
-    for i in range(N):
-        time.sleep(random_time_interval(0.5, 5.0))
+    for _ in range(N):
+        time.sleep(random_time_interval(0.5, 5.0))  
 
         led.high()
-
         tic = time.ticks_ms()
         t0 = None
+
+       
         while time.ticks_diff(time.ticks_ms(), tic) < on_ms:
-            if button.value() == 0:
+            if button.value() == 0:  
                 t0 = time.ticks_diff(time.ticks_ms(), tic)
                 led.low()
                 break
-        t.append(t0)
-
+       
+        t2.append(t0)
         led.low()
 
-    blinker(5, led)
+    blinker(led, 5)
 
-    scorer(t)
+   
+    data= scorer(t2)
+    print(f"saved in the json file.")
+
+   
+    upload_to_cloud(data)
